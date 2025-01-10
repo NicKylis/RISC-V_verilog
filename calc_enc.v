39707c3450fb030763a374900b239b86dc058374
NicKylis <kyliniko@ece.auth.gr>
2025-01-10 14:46:36 +0200

    clarifications

--- calc_enc.v
+++ calc_enc.v
@@ -5,6 +5,7 @@ module calc_enc (
     output wire [3:0] encout // 4-bit encoder output to match the ALU operation
 );
 
+// Logic imported from the diagrams
 assign encout[0] = (~btnc & btnr) | (btnr & btnl);
 assign encout[1] = (~btnl & btnc) | (btnc & ~btnr);
 assign encout[2] = ((btnc & btnr) | ((btnl & ~btnc) & ~btnr));
