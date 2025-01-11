cbde25cf2f1cbfb1d1951d57a47a7887b0d4b79f
NicKylis <kyliniko@ece.auth.gr>
2025-01-11 12:52:49 +0200

    Updated datapath with PC and rf

--- datapath.v
+++ datapath.v
@@ -11,12 +11,60 @@ module datapath #(parameter INITIAL_PC = 32'h00400000) (
     input MemToReg,
     input [3:0] ALUCtrl,
     input loadPC,
-    output [31:0] PC,
+    output reg [31:0] PC,
     output Zero,
     output [31:0] dAddress,
     output [31:0] dWriteData,
     output [31:0] dReadData,
     output [31:0] WriteBackData
 );
+
+// Program Counter
+
+// Branch instruction
+wire [31:0] branch_offset;
+assign branch_offset = {{14{instr[15]}}, instr[15:0]} << 2;
+
+// Next instruciton
+wire [31:0] next_PC;
+assign next_PC = PCSrc ? (PC + branch_offset) : (PC + 4);
+
+always @(posedge clk) begin
+    if (rst) begin
+        PC <= INITIAL_PC;
+    end else begin
+        PC <= next_PC;
+    end
+end
+
+// Register File
+
+// Decoded register addresses
+wire [4:0] readReg1;
+wire [4:0] readReg2;
+wire [4:0] writeReg;
+
+assign readReg1 = instr[25:21];
+assign readReg2 = instr[20:16];
+assign writeReg = instr[15:11];
+
+regfile #(.DATAWIDTH(32), .REGCOUNT(32)) regfile (
+    .clk(clk),
+    .readReg1(readReg1),
+    .readReg2(readReg2),
+    .writeReg(writeReg),
+    .writeData(dWriteData),
+    .write(RegWrite),
+    .readData1(),
+    .readData2()
+);
+
+// Immediate generation
+
+// ALU
+
+// Branch target
+
+// Write back
 
 endmodule
