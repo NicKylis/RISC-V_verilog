e6b59d5c255e118fe3a1acc99ff03376e02896fe
NicKylis <kyliniko@ece.auth.gr>
2025-01-13 13:16:08 +0200

    initialized ram & rom

--- top_proc.v
+++ top_proc.v
@@ -21,5 +21,53 @@ datapath #(.INITIAL_PC(INITIAL_PC)) datapath (
     .dWriteData(dWriteData)
 );
 
+wire [2:0] fsm_state;
+assign fsm_state = 3'b000;
+
+always @(posedge clk) begin
+    case(fsm_state)
+        3'b000: begin
+            // IF
+            // PC = PC + 4
+            // dAddress = PC
+            // MemRead = 1
+            // MemWrite = 0
+            // WriteBackData = 0
+            // Next state: 3'b001
+        end
+        3'b001: begin
+            // ID
+            // dAddress = 0
+            // MemRead = 0
+            // MemWrite = 0
+            // WriteBackData = 0
+            // Next state: 3'b010
+        end
+        3'b010: begin
+            // EX
+            // dAddress = 0
+            // MemRead = 0
+            // MemWrite = 0
+            // WriteBackData = 0
+            // Next state: 3'b011
+        end
+        3'b011: begin
+            // MEM
+            // dAddress = 0
+            // MemRead = 0
+            // MemWrite = 0
+            // WriteBackData = 0
+            // Next state: 3'b100
+        end
+        3'b100: begin
+            // WB
+            // dAddress = 0
+            // MemRead = 0
+            // MemWrite = 0
+            // WriteBackData = 0
+            // Next state: 3'b000
+        end
+    endcase
+end
 
 endmodule
